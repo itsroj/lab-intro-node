705df4231c64345d5ca42ac210887c95f1a96a03
Rojda Polat <rojdapolat@outlook.de>
2025-03-10 15:05:32 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0){
+      return 0;
+    } else {
+      const total = this.items.reduce((acc, currentEl) => acc + currentEl);
+      return total;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      const total = this.items.reduce((acc, currentEl) => acc + currentEl);
+      const avg = total / this.length;
+      return avg;
+    }
+  }
 }
 
 module.exports = SortedList;
